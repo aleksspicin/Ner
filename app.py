d5049a5826f3240485476b28b76ffe109e6f72ab
Александр <aspicin@hu-li.com>
2024-04-22 09:24:06 +0200

    update app

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from spacy import displacy
 nlp = spacy.load('nlp_model_big_1806')
 app = Flask(__name__)
 
-@app.route('/process',methods=["POST"])
+@app.route('/process',methods=["GET","POST"])
 def process():
     predict = nlp(request.form['test'])
     displacy.render(predict,style='ent')
     return predict.to_json()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
 
 
